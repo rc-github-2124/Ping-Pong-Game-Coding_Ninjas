dd21e43a2c757d9115cbd4f9a0b53e58abd41bb9
Rohit Chavan <78083751+rc-github-2124@users.noreply.github.com>
2023-04-27 18:33:48 +0530

    Add files via upload

---
+++ paddles.js
@@ -0,0 +1,16 @@
+class peddle{
+    constructor(x)
+    {
+        this.x=x;
+        this.y=200;
+        this.w=20;
+        this.h=100;
+        this.vy=0;
+    }
+    move(){
+
+    }
+    show(){
+        rect(this.x,this.y,this.w,this.h)
+    }
+}
